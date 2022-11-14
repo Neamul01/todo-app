5b6f933eefc6254c05d036afc3f0104722ff056f
Neamul01 <neamulhaque2002@gmail.com>
2022-11-14 20:54:58 +0600

    new component added

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="todo">
-    <h1>This is todo page</h1>
+    <h1>Todo's</h1>
+    <input type="text" name="todo" id="todo" class="todo" />
+    <button class="button">Add</button>
+    <div>
+      <ToDos />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import ToDos from "@/components/ToDos.vue";
+export default {
+  // name:'App',
+  components: {
+    ToDos,
+  },
+};
+</script>
+
+<style>
+.todo {
+  padding: 0.5rem;
+}
+.button {
+  padding: 0.5rem;
+}
+</style>
